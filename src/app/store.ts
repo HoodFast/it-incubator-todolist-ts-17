34f9abcce7f95ad52278bcd79c41c5aadf5a42d9
HoodFast <rabota-trassa@mail.ru>
2023-06-30 23:59:45 +0300

    commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import { tasksReducer } from "features/TodolistsList/tasks-reducer";
 import { todolistsReducer } from "features/TodolistsList/todolists-reducer";
-import { AnyAction, applyMiddleware, combineReducers, createStore } from "redux";
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { AnyAction, combineReducers } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "features/Login/auth-reducer";
 import { configureStore } from "@reduxjs/toolkit";
